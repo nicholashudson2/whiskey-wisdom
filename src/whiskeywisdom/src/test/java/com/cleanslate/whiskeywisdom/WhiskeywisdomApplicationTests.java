a3cc05d84262a575afef3c2050a78f9a2c214e0e
Nicholas Hudson <Nicholas Hudson>
2017-11-08 16:43:04 -0500

    Spring backend created, Security configuration pending.

---
+++ src/whiskeywisdom/src/test/java/com/cleanslate/whiskeywisdom/WhiskeywisdomApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cleanslate.whiskeywisdom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhiskeywisdomApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
