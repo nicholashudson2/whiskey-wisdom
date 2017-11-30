1e7448eddf9c27e9a2ddb82a66df89df1bc2d34c
Nicholas Hudson <nicholas.hudson2@yahoo.com>
2017-11-29 19:38:27 -0500

    Began working on implementing Quill rich-text editor. Still needs bug fixing.

--- ui/js/modules/session/new-post/newPost.module.js
+++ ui/js/modules/session/new-post/newPost.module.js
@@ -4,8 +4,38 @@ import newPostService from './newPost.service.js'
 
 export default
   angular
-    .module('newPost', [])
+    .module('newPost', ['ngQuill'])
     .component('newPostComponent', newPostComponent)
-    .controller('newPostController', newPostController)
+    .controller('newPostController', newPostController, [
+        '$scope',
+        '$timeout',
+        function ($scope, $timeout) {
+          $scope.title = 'Quill works'
+          $scope.model = ''
+          $scope.readOnly = false
+          $scope.test = ''
+          $scope.customOptions = [{
+            import: 'attributors/style/size',
+            whitelist: ['14', '16', '18', 'small', 'large', 'huge']
+          }]
+          $scope.customModules = {
+            toolbar: [
+              [{'size': [false, '14', '16', '18']}]
+            ]
+          }
+          $scope.editorCreated = function (editor) {
+            console.log(editor)
+          }
+          $scope.contentChanged = function (editor, html, text, delta, oldDelta, source) {
+            console.log('editor: ', editor, 'html: ', html, 'text:', text, 'delta: ', delta, 'oldDelta:', oldDelta, 'source:', source)
+          }
+          $scope.selectionChanged = function (editor, range, oldRange, source) {
+            console.log('editor: ', editor, 'range: ', range, 'oldRange:', oldRange, 'source:', source)
+          }
+        }
+      ])
     .service('newPostService', newPostService)
+    .config(['ngQuillConfigProvider', function (ngQuillConfigProvider) {
+      ngQuillConfigProvider.set(null, null, 'custom placeholder')
+    }])
     .name
