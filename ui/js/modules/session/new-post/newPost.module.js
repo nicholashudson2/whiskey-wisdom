ae351fe785e58e903b231149db0002c8cafd02eb
Nicholas Hudson <nicholas.hudson2@yahoo.com>
2017-11-29 20:10:29 -0500

    some restructuring of newPost components completed, still needs bug fixes.

--- ui/js/modules/session/new-post/newPost.module.js
+++ ui/js/modules/session/new-post/newPost.module.js
@@ -6,36 +6,6 @@ export default
   angular
     .module('newPost', ['ngQuill'])
     .component('newPostComponent', newPostComponent)
-    .controller('newPostController', newPostController, [
-        '$scope',
-        '$timeout',
-        function ($scope, $timeout) {
-          $scope.title = 'Quill works'
-          $scope.model = ''
-          $scope.readOnly = false
-          $scope.test = ''
-          $scope.customOptions = [{
-            import: 'attributors/style/size',
-            whitelist: ['14', '16', '18', 'small', 'large', 'huge']
-          }]
-          $scope.customModules = {
-            toolbar: [
-              [{'size': [false, '14', '16', '18']}]
-            ]
-          }
-          $scope.editorCreated = function (editor) {
-            console.log(editor)
-          }
-          $scope.contentChanged = function (editor, html, text, delta, oldDelta, source) {
-            console.log('editor: ', editor, 'html: ', html, 'text:', text, 'delta: ', delta, 'oldDelta:', oldDelta, 'source:', source)
-          }
-          $scope.selectionChanged = function (editor, range, oldRange, source) {
-            console.log('editor: ', editor, 'range: ', range, 'oldRange:', oldRange, 'source:', source)
-          }
-        }
-      ])
+    .controller('newPostController', newPostController)
     .service('newPostService', newPostService)
-    .config(['ngQuillConfigProvider', function (ngQuillConfigProvider) {
-      ngQuillConfigProvider.set(null, null, 'custom placeholder')
-    }])
     .name
