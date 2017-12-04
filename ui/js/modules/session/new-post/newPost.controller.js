0cbeedffcdc98b293af0fd8ca7280beb94d50b80
Nicholas Hudson <nicholas.hudson2@yahoo.com>
2017-12-04 12:53:33 -0500

    Having trouble running Maven build in new IDE, changes to class names from 'Episode' to 'Article' not in this branch. Going to merge/pull with master, should resolve build failures.

--- ui/js/modules/session/new-post/newPost.controller.js
+++ ui/js/modules/session/new-post/newPost.controller.js
@@ -4,28 +4,34 @@ const newPostController = ['newPostService', '$state', '$scope', function (newPo
     this.$state = $state
     this.$scope = $scope
 
-    $scope.title = ''
+    $scope.content = ''
 
     $scope.readOnly = false
     $scope.test = ''
     $scope.customOptions = [{
-      import: 'attributors/style/size',
-      whitelist: ['14', '16', '18', 'small', 'large', 'huge']
+        import: 'attributors/style/size',
+        whitelist: ['14', '16', '18', 'small', 'large', 'huge']
     }]
     $scope.customModules = {
-      toolbar: [
-        [{'size': [false, '14', '16', '18']}]
-      ]
+        toolbar: [
+            [{'size': [false, '14', '16', '18']}]
+        ]
     }
 
     $scope.editorCreated = function (editor) {
-      console.log(editor)
+        console.log('editor: ', editor)
     }
     $scope.contentChanged = function (editor, html, text, delta, oldDelta, source) {
-      console.log('editor: ', editor, 'html: ', html, 'text:', text, 'delta: ', delta, 'oldDelta:', oldDelta, 'source:', source)
+        $scope.content = html
+        console.log('editor: ', editor, 'html: ', html, 'text:', text, 'delta: ', delta, 'oldDelta:', oldDelta, 'source:', source)
     }
     $scope.selectionChanged = function (editor, range, oldRange, source) {
-      console.log('editor: ', editor, 'range: ', range, 'oldRange:', oldRange, 'source:', source)
+        console.log('editor: ', editor, 'range: ', range, 'oldRange:', oldRange, 'source:', source)
+    }
+    $scope.saveClick = () => {
+        console.log($scope.content)
+        console.log($scope.display.enabled)
+        newPostService.postNewArticle($scope.content, $scope.display.enabled, false)
     }
 }]
 
